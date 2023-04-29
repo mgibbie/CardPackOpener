30ed5ed833c1322b49d05cb3e177d6b976818f87
Michael Gibalerio <michaelgibalerio@Michaels-MacBook.local>
2023-04-29 15:06:23 -0400

    initialize

---
+++ app.js
@@ -0,0 +1,32 @@
+
+const API_ENDPOINT = "https://api.scryfall.com/cards/random?q=game%3Amtg";
+const CARD_COUNT = 15;
+
+
+const revealButton = document.getElementById("reveal-cards-button");
+const cardContainer = document.getElementById("card-container");
+
+
+revealButton.addEventListener("click", async () => {
+
+  revealButton.disabled = true;
+
+
+  cardContainer.innerHTML = "";
+
+  for (let i = 0; i < CARD_COUNT; i++) {
+    try {
+      const response = await fetch(API_ENDPOINT);
+      const { image_uris } = await response.json();
+      const cardImage = document.createElement("img");
+      cardImage.className = "card-image";
+      cardImage.src = image_uris.normal;
+      cardImage.alt = "Magic: The Gathering card";
+      cardContainer.appendChild(cardImage);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  revealButton.disabled = false;
+});
